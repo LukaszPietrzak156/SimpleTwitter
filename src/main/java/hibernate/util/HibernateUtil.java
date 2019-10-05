59181ce73c904846d89a67c0dcc92d9b9716bdc3
LukaszPietrzak156 <pietrzakl90@gmail.com>
2019-10-05 15:04:16 +0200

    add methods

--- src/main/java/hibernate/util/HibernateUtil.java
+++ src/main/java/hibernate/util/HibernateUtil.java
@@ -6,6 +6,7 @@ import org.hibernate.Transaction;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
 
 public class HibernateUtil {
     private static HibernateUtil instance;
@@ -22,11 +23,32 @@ public class HibernateUtil {
     }
 
     public void saveByHibernateSession(Object o) {
-        Session session = entityManager.unwrap(Session.class);
-        Transaction transaction = session.beginTransaction();
-
+        try {
+            Session session = entityManager.unwrap(Session.class);
+            Transaction transaction = session.beginTransaction();
+            session.save(o);
+            transaction.commit();
+            session.close();
+        } catch (PersistenceException e){
+            System.out.printf("Problem with unwraping session occured :" + e);
+        }
     }
 
+    public void save(Object o){
+        entityManager.getTransaction().begin();
+        if(!entityManager.contains(o)){
+            entityManager.persist(o);
+            entityManager.flush();
+        }
+        entityManager.getTransaction().commit();
+    }
+
+    public void delete(Class clazz, Long objectId){
+        entityManager.getTransaction().begin();
+        Object toRemove = entityManager.find(clazz, objectId);
+        entityManager.remove(toRemove);
+        entityManager.getTransaction().commit();
+    }
 
 
 }
